9cc327250e39cf45a3efd8b780725afaedf636aa
oqo0 <oq.roman@gmail.com>
2024-05-22 18:18:39 +0300

    Change exception type

--- src/Samples/Samples.Simple/Modules/SampleModule.cs
+++ src/Samples/Samples.Simple/Modules/SampleModule.cs
@@ -7,6 +7,6 @@ public class SampleModule : InteractionModuleBase<SocketInteractionContext>
     [SlashCommand("sample", "Sample command")]
     public async Task SampleCommand(string message, string test)
     {
-        throw new NotImplementedException();
+        throw new InvalidOperationException("This is a sample bot");
     }
 }
